75db10f446580cea3eea49ee50ab7149566e6c02
Bello Yakub <hyaaquub@gmail.com>
2021-06-30 14:17:37 +0100

    added business model and business data class

---
+++ app/src/main/java/com/example/biographyapp/BusinessData.kt
@@ -0,0 +1,65 @@
+package com.example.biographyapp
+
+object BusinessData {
+    val Biography = listOf(
+            BusinessModel(name ="Aliko Dangote",
+            R.drawable.aliko,
+            biography = "Aliko Dangote is an ethnic Hausa Muslim from Kano, Kano State, was born on 10 April 1957 " +
+                    "into a wealthy Muslim family of the Nigerian bourgeoisie, the son of Mohammed Dangote and Mariya" +
+                    " Sanusi Dantata, the daughter of Sanusi Dantata. He is the great-grandson of Alhassan Dantata, the" +
+                    " richest West African at the time of his death in 1955. He is the wealthiest person in Africa, with " +
+                    "an estimated net worth of US$11.1 billion as of May 2021.\n" +
+                    "Dangote was educated at the Sheikh Ali Kumasi Madrasa, followed by Capital High School, Kano. In 1978," +
+                    " he graduated from the Government College, Birnin Kudu. He received a bachelor's degree in business studies" +
+                    " and administration from Al-Azhar University, Cairo.\n" +
+                    "The Dangote Group was established as a small trading firm in 1977, the same year Dangote relocated to Lagos" +
+                    " to expand the company. Today, it is a multi-trillion-naira conglomerate with many of its operations in Benin, " +
+                    "Ghana, Nigeria, Zambia and Togo. Dangote has expanded to cover food processing, cement manufacturing, and freight. " +
+                    "The Dangote Group also dominates the sugar market in Nigeria and is a major supplier to the country's soft drink" +
+                    " companies, breweries, and confectioners. The Dangote Group has moved from being a trading company to be the largest" +
+                    " industrial group in Nigeria including Dangote Sugar Refinery, Dangote Cement, and Dangote Flour.\n"),
+
+            BusinessModel(name = "Nicky Oppenheinmer",
+            R.drawable.oppenheimer, biography = "Nicholas F. Oppenheimer is a South African billionaire businessman who was born on 8th" +
+                    " June 1945. He is the son of Bridget (née McCall) and Harry Oppenheimer, and grandson of Anglo American founder Ernest" +
+                    " Oppenheimer (the first generation to chair De Beers, from 1929). His father was of German Jewish descent.  He was formerly" +
+                    "the chairman of De Beers diamond mining company and of its subsidiary, the Diamond Trading Company, and former deputy " +
+                    "chairman of Anglo American. He is the third richest man in Africa with an estimated net worth of US$ 8.1 billion as of May 2021.\n" +
+                    "Oppenheimer was educated at Harrow School and Christ Church, Oxford, where he read Philosophy, Politics and Economics," +
+                    " earning an Oxford MA. In 1968\n" + "Oppenheimer joined Anglo American and was appointed Director in 1974, then became deputy" +
+                    " chairman in 1983. He resigned in 2001, remaining a non-executive director until 2011. He was appointed deputy chairman of" +
+                    " the then Central Selling Organisation (now Diamond Trading Company) in 1984, and deputy chairman of De Beers Consolidated" +
+                    " Mines in 1985. He was also appointed chairman of the Diamond Trading Company in 1985. Chairman of the De Beers Group from" +
+                    " 1998 to 2012, he retired when the family stake was sold to Anglo American"),
+
+            BusinessModel(name = " Mike Adenuga",
+            R.drawable.adenuga, biography = "This is Mike Adenuga biography"),
+
+            BusinessModel(name = "Nassef Sawiris",
+            R.drawable.sawiris, biography = "This is Nassef Sawiris biography"),
+
+            BusinessModel(name = "Johann Rupert",
+            R.drawable.johann, biography = "This is Johann Rupert biography"),
+
+            BusinessModel(name = "Issad Rebrad",
+            R.drawable.rebrab, biography = "This is Issad Rebrad biography"),
+
+            BusinessModel(name = "Mohamed Mansour",
+            R.drawable.monsour, biography = "This is Mohamed Mansour biography"),
+
+            BusinessModel(name = "Naguib Sawiris",
+            R.drawable.naguib,
+                    biography = "This is Naguib Sawiris biography"),
+
+            BusinessModel(name = "Patrice Motsepe",
+            R.drawable.motsepe,
+            biography = "This is Patrice Motsepe Biography"),
+
+            BusinessModel(name = "Isabel dos Santos",
+            R.drawable.isabel,
+            biography = "This is Isabel dos Santos biography")
+
+
+
+    )
+}
---
+++ app/src/main/java/com/example/biographyapp/BusinessModel.kt
@@ -0,0 +1,3 @@
+package com.example.biographyapp
+
+data class BusinessModel(val name:String, val image:Int, val biography:String)
